9e90c276b7f9ae3a5779a47027c8bd6aa2b12d90
ParkerHansen7777 <parkerhansen7777@gmail.com>
2025-08-05 19:02:51 -0700

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -12,6 +12,8 @@ app = Flask(__name__)
 cors = CORS(app) # allow CORS for all domains on all routes.
 app.config['CORS_HEADERS'] = 'Content-Type'
 
+@app.route("/")
+@cross_origin()
 
 # Route for seeing a data
 @app.route('/teams')
